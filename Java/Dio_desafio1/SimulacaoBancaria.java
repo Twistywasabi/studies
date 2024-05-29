fc3ff68768599d3a55383b1f914ae5d4bf59f2b7
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-05-29 16:13:19 -0300

    SimulacaoBancaria.java >> resolução switch case 1 e switch case 2

--- Java/Dio_desafio1/SimulacaoBancaria.java
+++ Java/Dio_desafio1/SimulacaoBancaria.java
@@ -16,10 +16,22 @@ public class SimulacaoBancaria {
                 case 1:
                     // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
                     System.out.println("Opção 1 selecionada");
+                    System.out.println("Qual será o valor a ser depositado ?");
+                    int valorDepositado = scanner.nextInt();
+                    saldo = saldo + valorDepositado;
+                    System.out.println("Saldo Atual: " + saldo);
                     break;
                 case 2:
                     // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
                     System.out.println("Opção 2 selecionada");
+                    System.out.println("Qual será o valor a ser sacado ?");
+                    int valorSacado = scanner.nextInt();
+                    if(saldo < valorSacado) {
+                        System.out.println("Saldo insuficiente");
+                    } else {
+                        saldo = saldo - valorSacado;
+                        System.out.println("Saldo Atual: " + saldo);
+                    }
                     break;
                 case 3:
                     // TODO: Exibir o saldo atual da conta.
