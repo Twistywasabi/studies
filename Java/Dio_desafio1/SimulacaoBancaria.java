3ec1a23c6ea5395e46a711d1369eae63baa2caec
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-05-29 15:59:02 -0300

    SimulacaoBancaria.java >> adição de System.out.println para verificação de funcionalidades de entrada, processamento e saída

--- Java/Dio_desafio1/SimulacaoBancaria.java
+++ Java/Dio_desafio1/SimulacaoBancaria.java
@@ -9,17 +9,21 @@ public class SimulacaoBancaria {
 
         while (continuar) {
 
+            System.out.println("Escolha uma opção: 1 - Depositar // 2 - Sacar // 3 - Consultar Saldo // 0 - Encerrar");
             int opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
                     // TODO: Ler o valor a ser depositado e atualizar/imprimir o saldo.
+                    System.out.println("Opção 1 selecionada");
                     break;
                 case 2:
                     // TODO: Ler o valor a ser sacado e verificar/imprimir se há saldo suficiente.
+                    System.out.println("Opção 2 selecionada");
                     break;
                 case 3:
                     // TODO: Exibir o saldo atual da conta.
+                    System.out.println("Opção 3 selecionada");
                     break;
                 case 0:
                     System.out.println("Programa encerrado.");
