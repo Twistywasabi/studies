6ba7d10572913df3bf71dc8324354f2365371aa6
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-05-31 19:45:09 -0300

    Sum of right side of index

--- codewars/equalSidesOfAnArray.js
+++ codewars/equalSidesOfAnArray.js
@@ -1,6 +1,25 @@
 function findEvenIndex(arr) {
   //Code goes here!
+  //Pega o array
+
   console.log(arr);
+
+  //Coloca em um loop que vai interagir com cada elemento do array
+  for (let i = 0; i < arr.length; i++) {
+    ////Pega o indice atual e veja os elementos da direita e faz a soma
+    const rightArray = arr.slice(i);
+    console.log(rightArray);
+    const sumRightArrayInitialValue = 0;
+    const sumRightArray = rightArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      sumRightArrayInitialValue
+    );
+    console.log(sumRightArray);
+    ////Pega o indice atual e veja os elementos da esquerda e faz a soma
+    ////compara a soma dos indices da direita com os indices da esqueda
+  }
+  //Se achou, imprimir resultado(no caso indice 3)
+  //Se não, colocar indice -1
 }
 
-findEvenIndex([0, 1, 2, 3]);
+findEvenIndex([1, 2, 3, 4, 3, 2, 1]);
