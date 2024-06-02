d401362121f2e6dcc0dfa2e09b71785a48f31978
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-06-01 22:02:18 -0300

    Ajustes e testes finais

--- codewars/equalSidesOfAnArray.js
+++ codewars/equalSidesOfAnArray.js
@@ -2,29 +2,30 @@ function findEvenIndex(arr) {
   //Code goes here!
   //Pega o array
   //Coloca em um loop que vai interagir com cada elemento do array
+  let answerIndex = -1;
   for (let i = 0; i < arr.length; i++) {
     ////Pega o indice atual e veja os elementos da direita e faz a soma
     const rightArray = arr.slice(i + 1);
-    console.log(rightArray);
     const sumRightArrayInitialValue = 0;
     const sumRightArray = rightArray.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       sumRightArrayInitialValue
     );
-    console.log(sumRightArray);
     ////Pega o indice atual e veja os elementos da esquerda e faz a soma
     const leftArray = arr.slice(-arr.length, i);
-    console.log(leftArray);
     const sumLeftArrayInitialValue = 0;
     const sumLeftArray = leftArray.reduce(
       (accumulator, currentValue) => accumulator + currentValue,
       sumLeftArrayInitialValue
     );
-    console.log(sumLeftArray);
     ////compara a soma dos indices da direita com os indices da esqueda
+    if (sumLeftArray === sumRightArray) {
+      answerIndex = i;
+      break;
+    }
   }
+  return answerIndex;
   //Se achou, imprimir resultado(no caso indice 3)
   //Se não, colocar indice -1
 }
-
-findEvenIndex([1, 2, 3, 4, 5, 6, 7]);
+findEvenIndex([1, 2, 3, 4, 3, 2, 1]);
