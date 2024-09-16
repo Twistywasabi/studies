df0b32affa757bc6d2a2d79db40c99832ed290c2
TwistyWasabi17_LG <lucas-hideaki@hotmail.com>
2024-09-16 15:14:47 -0300

    exercicio PaginationHelper

---
+++ codewars/paginationHelper.js
@@ -0,0 +1,29 @@
+class PaginationHelper {
+  constructor(collection, itemsPerPage) {
+    // The constructor takes in an array of items and a integer indicating how many
+    // items fit within a single page
+    for (let index = 0; index < collection.length; index++) {
+      const element = array[index];
+      console.llog(element);
+    }
+  }
+
+  itemCount() {
+    // returns the number of items within the entire collection
+  }
+  pageCount() {
+    // returns the number of pages
+  }
+  pageItemCount(pageIndex) {
+    // returns the number of items on the current page. page_index is zero based.
+    // this method should return -1 for pageIndex values that are out of range
+  }
+  pageIndex(itemIndex) {
+    // determines what page an item is on. Zero based indexes
+    // this method should return -1 for itemIndex values that are out of range
+  }
+}
+
+const teste = new PaginationHelper();
+
+con;
